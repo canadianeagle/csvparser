929a977e6d6d9287843b18697e03fbb6437eb8b4
Tonye Brown <bzllbrown@gmail.com>
2020-02-12 20:26:50 -0800

    csv parser done

--- README.md
+++ README.md
@@ -1,68 +1,38 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<p align="center">
+  <a href="http://csv.tonyebrown.com/">
+    <img alt="Tonye Brown new logo" style="filter: invert(1)" src="http://3.bp.blogspot.com/-GjqjxCgQ3s4/WrIFh_DvnJI/AAAAAAAAoec/Vnf6_L_VoD8HGXGrY2P897KGmmhHlk40QCK4BGAYYCw/s1600/tbb%2Blogo%2B4%2Bwhite%2B2%2Bsmall.png" width="98" />
+  </a>
+</p>
+<h1 align="center">
+  Welcome to my CSV Parser App
+</h1>
 
-## Available Scripts
 
-In the project directory, you can run:
+## 🚀 Technologies used 
 
-### `yarn start`
+1.  **Technologies used**
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+    I'm using React and basic CSS. No SASS :/.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+    ```shell
+    # "It’s not where you take things from - it’s where you take them to."
+    - Jean-Luc Godard
+    ```
+1.  **Features**
+    - Parses .csv and calculates based on San Francisco
 
-### `yarn test`
+1.  **Future Enhancements**
+    - Add animations and images
+    - Add starting location/city
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+1.  **Quick stop for me**
 
-### `yarn build`
+    Local app will be running on `http://localhost:3000`!
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+    _Note: You'll also see a second link: _`http://localhost:8000/___graphql`_. This is a tool you can use to experiment with querying your data. Learn more about using this tool in the [Gatsby tutorial](https://www.gatsbyjs.org/tutorial/part-five/#introducing-graphiql)._
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Thanks for visiting, and thanks for reading.
 
-### `yarn eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+All Rights Reserved. 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const inputOpenFileRef = useRef(null);
+  let fileReader;
+  
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+
+    let final = [];
+    let split = [];
+    let reversed = [];
+    let lines = [];
+    lines = content.split(/\r\n|\n/); //split text into lines
+
+    for (let i=0; i<lines.length; i++) { //split lines into arrays
+      if(lines[i] !== "") {
+        split.push(lines[i].split(","));//.reverse());
+        reversed.push(lines[i].split(",").reverse());
+      }
+    }
+
+    let compare = 0;
+
+    for (let i=0; i<split.length; i++) { // find san fran
+      if(split[i].includes("San Francisco")) {
+        final.push(split[i][0]);
+        compare = split[i][3];
+      }
+    }
+
+    let sorted = reversed.sort((a, b) => a[0] - b[0]).reverse();
+    let index = sorted.length-1;
+    
+    while(index >= 0) { // add cities to route
+      if(!sorted[index].includes("San Francisco")) { 
+        if (parseFloat(sorted[index][0]) > parseFloat(compare) && !final.includes(sorted[index][0])) {
+          final.push(sorted[index][3]);
+        }
+      }
+      index--;
+    }  
+
+    let tempDist = 0;
+    for (let i=0; i<sorted.length; i++) { // calc distance
+      if (i<sorted.length-1) {
+        tempDist += calcDistance(sorted[i][1], sorted[i][0], sorted[i+1][1], sorted[i+1][0])
+      }
+    }
+
+    setDistance(parseFloat(tempDist).toFixed(0));
+    setOutput(final);
+
+  };
+
+  const calcDistance = (lat1, lon1, lat2, lon2) => {
+    
+    var p = 0.017453292519943295;
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((lon2 - lon1) * p))/2;
+  
+    return 12742 * Math.asin(Math.sqrt(a)); 
+  }
+
+  const parseCSV = (e) => {
+    if(e.target.files[0]){
+      setFile(e.target.files[0]);
+      fileReader = new FileReader();
+      fileReader.onloadend = handleFileRead;
+      fileReader.readAsText(e.target.files[0]);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Browse for your csv file:</h1>
+        <div>
+          <span className="control-fileupload mt-3">
+            <label htmlFor="file">{file === null ? "Choose a .csv file to parse:" : file.name}</label>
+            <input
+              accept=".csv"
+              autoComplete="off"
+              className="custom-file-input"
+              onChange={(e) => { parseCSV(e) }}
+              placeholder="Choose a csv file to parse"
+              ref={inputOpenFileRef}
+              type="file"
+            />
+          </span>
+          <a href="/cities_small.csv"><h3>Sample CSV File</h3></a>
+        </div>
+        
+        <div className="output mt-5 p-4 border">
+          <h4>Suggested Route</h4>
+          {
+            output === null ?
+              <span><h2>No file selected yet.</h2><br/><br/>
+                This app parses a file containing cities with longitude and latitudes and sorts them based on what's closest to San Francisco.<br/><br/><br/>
+                If you need a sample csv file, see the link above. 
+              </span>
+            :
+            <p>
+              {
+                output.map((city) => 
+                  <><span key={city}>{city}</span><br /></>
+                )
+              }
+              <span key="static">{output[0]}</span><br />
+            </p>
+          }
+        </div>
+        <div className="total mt-3 p-4 border">
+          <h5>Distance travelled</h5>
+          { distance === null ? 0+" km" : distance+" km" }
+        </div>
       </header>
     </div>
   );
